98da77c326f3bd4129d354ea6a0b29e62d5e2812
Vegard Gamnes <vegard@gamnes.no>
2024-02-05 21:12:33 +0100

    Terminal border

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local Util = require("lazyvim.util")
+
+vim.keymap.set("n", "<leader>ft", function()
+  Util.terminal(nil, { cwd = Util.root.get(), border = "rounded" })
+end, { desc = "Terminal (root)" })
+
+vim.keymap.set("n", "<leader>fT", function()
+  Util.terminal(nil, { border = "rounded" })
+end, { desc = "Terminal (cwd)" })
