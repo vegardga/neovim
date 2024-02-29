527850dc529879179dfc6a3dbc030d13ed220785
Vegard Gamnes <vegard@gamnes.no>
2024-02-29 20:15:11 +0100

    Add options to silicon screenshots

--- lua/plugins/code-screenshots.lua
+++ lua/plugins/code-screenshots.lua
@@ -6,6 +6,8 @@ return {
     require("silicon").setup({
       -- Configuration here, or leave empty to use defaults
       font = "MesloLGS NF=34;Noto Emoji=34",
+      theme = "Dracula",
+      background = "#FFFFFF",
     })
     vim.keymap.set("n", "<leader>cs", ":Silicon<CR>", { desc = "Silicon screenshot" })
     vim.keymap.set("v", "<leader>cs", ":Silicon<CR>", { desc = "Silicon screenshot" })
